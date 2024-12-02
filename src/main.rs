0ca750c54a4e6ae1fdb317f7dd0c104d7491aa8b
Pierre <66693681+extua@users.noreply.github.com>
2024-12-02 16:17:49 +0000

    pass url parameters in query

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,8 @@
 #[tokio::main]
 async fn main() -> Result<(), reqwest::Error> {
     let echo_json: serde_json::Value = reqwest::Client::new()
-        .get("https://public.api.bsky.app/xrpc/app.bsky.feed.getAuthorFeed?actor=did:plc:blxilps4iwbxicionf2rztej")
+        .get("https://public.api.bsky.app/xrpc/app.bsky.feed.getAuthorFeed")
+        .query(&[("actor", "did:plc:blxilps4iwbxicionf2rztej")])
         .send()
         .await?
         .json()
