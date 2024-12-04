0377d7cf42e83c814c119b2b32ad45472b76ea18
Pierre Marshall <pierre.marshall@gmail.com>
2024-12-04 12:36:09 +0000

    use bsky search

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,13 @@
 #[tokio::main]
 async fn main() -> Result<(), reqwest::Error> {
     let echo_json: serde_json::Value = reqwest::Client::new()
-        .get("https://public.api.bsky.app/xrpc/app.bsky.feed.getAuthorFeed")
-        .query(&[("actor", "did:plc:blxilps4iwbxicionf2rztej")])
+        .get("https://public.api.bsky.app/xrpc/app.bsky.feed.searchPosts")
+        .query(&[("q", "Oxford"), ("limit", "4")])
         .send()
         .await?
         .json()
         .await?;
 
-    println!("{}",echo_json["feed"][0]["post"]["record"]["text"]);
+    println!("{:#?}",echo_json);
     Ok(())
 }
