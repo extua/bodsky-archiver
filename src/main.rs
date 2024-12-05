5db18b599466b700ade68d83277b392f7e559e63
Pierre <66693681+extua@users.noreply.github.com>
2024-12-05 12:48:53 +0000

    rewrite main.rs to call the bluesky firehose

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,33 @@
-#[tokio::main]
-async fn main() -> Result<(), reqwest::Error> {
-    let echo_json: serde_json::Value = reqwest::Client::new()
-        .get("https://public.api.bsky.app/xrpc/app.bsky.feed.searchPosts")
-        .query(&[("q", "Oxford"), ("limit", "4")])
-        .send()
-        .await?
-        .json()
-        .await?;
+use tungstenite::{
+    connect,
+    protocol::{frame::coding::CloseCode, CloseFrame},
+    Message,
+};
+// use minicbor::Decode;
 
-    println!("{:#?}",echo_json);
-    Ok(())
+
+fn main() {
+    env_logger::init();
+
+    let (mut socket, response) =
+        connect("wss://bsky.network/xrpc/com.atproto.sync.subscribeRepos").expect("Can't connect");
+
+    println!("Connected to the server");
+    println!("Response HTTP code: {}", response.status());
+    println!("Response contains the following headers:");
+    for (header, _value) in response.headers() {
+        println!("* {header}");
+    }
+
+    for _ in 0..1 {
+        let msg: Message = socket.read().expect("Error reading message");
+        // let msg_txt = msg.to_string();
+        // let output: [&str; 2] = minicbor::decode(msg)?;
+
+        println!("Received: {:?}", output);
+    }
+    let _ = socket.close(Some(CloseFrame {
+        code: (CloseCode::Normal),
+        reason: ("finished".into()),
+    }));
 }
