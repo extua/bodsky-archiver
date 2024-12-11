6eb6fdca3b2d730fca00c40566c7208da6d83d88
Pierre Marshall <pierre.marshall@gmail.com>
2024-12-11 12:00:27 +0000

    chore: rewrote and tidied up the get_posts_number function

--- examples/fetch-dids/main.rs
+++ examples/fetch-dids/main.rs
@@ -1,50 +1,28 @@
 use core::panic;
-use std::error::Error;
-use serde::Deserialize;
+mod profile;
 
-use serde_json::{Number, Value};
 const ACCOUNT_DID: &str = "did:plc:blxilps4iwbxicionf2rztej";
 
-#[tokio::main]
-async fn get_posts_number() -> Result<Value, reqwest::Error> {
-    // #[derive(Deserialize)]
-    // struct postsCount {
-    //     postsCount: Number,
-    // }
-
-    let echo_json: serde_json::Value = reqwest::Client::new()
-        .get("https://public.api.bsky.app/xrpc/app.bsky.actor.getProfile")
-        .query(&[("actor", ACCOUNT_DID)])
-        .send()
-        .await?
-        .json()
-        .await?;
-
-    Ok(echo_json)
+fn get_posts_number() -> u64 {
+    #[tokio::main]
+    async fn request_posts_number() -> Result<profile::Root, reqwest::Error> {
+        let raw_response: Result<profile::Root, reqwest::Error> = reqwest::Client::new()
+            .get("https://public.api.bsky.app/xrpc/app.bsky.actor.getProfile")
+            .query(&[("actor", ACCOUNT_DID)])
+            .send()
+            .await?
+            .json::<profile::Root>()
+            .await;
+        raw_response
+    }
+    let response: profile::Root = match request_posts_number() {
+        Ok(file) => file,
+        Err(error) => panic!("Failed to get or parse API response: {error:?}"),
+    };
+    response.posts_count
 }
-
-// let post_count: Option<u64> = get_posts_number()["postsCount"].as_u64();
 
 fn main() {
-    let post_count = get_posts_number().expect("fasfsafa");
-
-    // let post_count: u64 = match get_posts_number() {
-    //     Some (post_count) => post_count,
-    //     None => 0u64,
-    //     };
-    println!("{}", post_count);
+    let post_count = get_posts_number();
+    println!("{:?}", post_count);
 }
-
-// let echo_json: serde_json::Value = reqwest::Client::new()
-//     .get("https://public.api.bsky.app/xrpc/app.bsky.feed.getAuthorFeed")
-//     .query(&[("actor", "did:plc:blxilps4iwbxicionf2rztej"), ("limit", "100"), ("cursor", "2023-11-28T11:40:31.232Z")])
-//     .send()
-//     .await?
-//     .json()
-//     .await?;
-
-// println!("{echo_json:#?}");
-// Ok(())
-// }
-
-// function returns object, cursor
