dd3b3873cd5c00c189b4132b058b4c759b86700c
Pierre Marshall <pierre.marshall@gmail.com>
2024-12-11 12:18:06 +0000

    chore: remove unwieldy struct, now inlined in get_posts_number

--- examples/fetch-dids/main.rs
+++ examples/fetch-dids/main.rs
@@ -1,21 +1,27 @@
 use core::panic;
-mod profile;
+use serde::{Deserialize, Serialize};
 
 const ACCOUNT_DID: &str = "did:plc:blxilps4iwbxicionf2rztej";
 
 fn get_posts_number() -> u64 {
+    #[derive(Serialize, Deserialize)]
+    #[serde(rename_all = "camelCase")]
+    pub struct Profile {
+        posts_count: u64,
+    }
+
     #[tokio::main]
-    async fn request_posts_number() -> Result<profile::Root, reqwest::Error> {
-        let raw_response: Result<profile::Root, reqwest::Error> = reqwest::Client::new()
+    async fn request_posts_number() -> Result<Profile, reqwest::Error> {
+        let raw_response: Result<Profile, reqwest::Error> = reqwest::Client::new()
             .get("https://public.api.bsky.app/xrpc/app.bsky.actor.getProfile")
             .query(&[("actor", ACCOUNT_DID)])
             .send()
             .await?
-            .json::<profile::Root>()
+            .json::<Profile>()
             .await;
         raw_response
     }
-    let response: profile::Root = match request_posts_number() {
+    let response: Profile = match request_posts_number() {
         Ok(file) => file,
         Err(error) => panic!("Failed to get or parse API response: {error:?}"),
     };
