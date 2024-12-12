ec2d0b579505cb52a7c1b734b9f23ecb7ba0486c
extua <pierre.marshall@gmail.com>
2024-12-12 12:32:45 +0000

    feat: return responses as a fixed-length array

--- src/main.rs
+++ src/main.rs
@@ -28,23 +28,42 @@ fn get_posts_number() -> u64 {
     response.posts_count
 }
 
-#[tokio::main]
-async fn request_posts_from_api() -> Result<Value, reqwest::Error> {
+fn collect_api_responses() {
+    #[tokio::main]
+    async fn request_posts_from_api() -> [Value; config::POSTS_PER_REQUEST] {
+        #[derive(Serialize, Deserialize, Debug)]
+        #[serde(rename_all = "camelCase")]
+        pub struct AuthorFeed {
+            feed: [Value; config::POSTS_PER_REQUEST],
+        }
 
-    let posts_per_request_str: String = config::POSTS_PER_REQUEST.to_string();
-    let raw_response: Result<Value, reqwest::Error> = reqwest::Client::new()
-        .get("https://public.api.bsky.app/xrpc/app.bsky.feed.getAuthorFeed")
-        .query(&[("actor", config::ACCOUNT_DID), ("limit", &posts_per_request_str)])
-        .send()
-        .await?
-        .json::<Value>()
-        .await;
-    raw_response
+        let posts_per_request_str: String = config::POSTS_PER_REQUEST.to_string();
+
+        let raw_response: Result<AuthorFeed, reqwest::Error> = reqwest::Client::new()
+            .get("https://public.api.bsky.app/xrpc/app.bsky.feed.getAuthorFeed")
+            .query(&[
+                ("actor", config::ACCOUNT_DID),
+                ("limit", &posts_per_request_str),
+            ])
+            .send()
+            .await
+            .unwrap()
+            .json::<AuthorFeed>()
+            .await;
+        let response: AuthorFeed = match raw_response {
+            Ok(response) => response,
+            Err(error) => panic!("Failed to get or parse API response: {error:?}"),
+        };
+        let feed: [Value; config::POSTS_PER_REQUEST] = response.feed;
+        feed
+    }
 }
 
 fn main() {
-    let api_loops_needed: u64 = get_posts_number().div_euclid(config::POSTS_PER_REQUEST) + 1;
+    let api_loops_needed: u64 =
+        get_posts_number().div_euclid(config::POSTS_PER_REQUEST.try_into().unwrap()) + 1;
     println!("{}", api_loops_needed);
-    let posts: Value = request_posts_from_api().unwrap();
-    println!("uri {}", posts["feed"][0]["post"]["uri"]);
+    let posts = request_posts_from_api();
+
+    println!("uri {:#?}", posts);
 }
