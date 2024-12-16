0e532998687f952d02380e0cf1cf28b2871f6073
Pierre Marshall <pierre.marshall@bodleian.ox.ac.uk>
2024-12-16 14:37:16 +0000

    refactor: move this url parsing logic into the api calling loop

--- src/main.rs
+++ src/main.rs
@@ -30,7 +30,7 @@ fn get_posts_number() -> usize {
     response.posts_count
 }
 
-fn collect_api_responses(total_posts: usize) {
+fn collect_api_responses(total_posts: usize) -> Vec<String> {
     // This loop tracks the number of posts remaining and the number
     // to make in each api call
     let api_calls_needed: usize = total_posts.div_euclid(config::POSTS_PER_REQUEST) + 1;
@@ -38,16 +38,20 @@ fn collect_api_responses(total_posts: usize) {
     let mut posts_remaining: usize = total_posts;
     let mut posts_to_request: usize = config::POSTS_PER_REQUEST;
     let mut cursor: String = "".to_string();
+    let mut feed: Vec<String> = Vec::with_capacity(total_posts);
     while current_call <= api_calls_needed {
         if posts_remaining < config::POSTS_PER_REQUEST {
             posts_to_request = posts_remaining
         }
         println!("requesting {} posts", posts_to_request);
         let bulk_posts: AuthorFeed = request_bulk_posts_from_api(posts_to_request, &cursor);
+        // update the cursor value
         cursor = bulk_posts.cursor;
-        println!("{}", &cursor);
-    
-    
+        for post in bulk_posts.feed {
+            let at_uri: &str = post["post"]["uri"].as_str().unwrap();
+            let http_url: String = convert_at_uri_to_url(at_uri);
+            feed.push(http_url);
+        }
         current_call += 1;
         posts_remaining -= posts_to_request;
     }
@@ -82,21 +86,12 @@ fn collect_api_responses(total_posts: usize) {
         response
     }
 
-    // fn parse_urls_from_posts() -> Vec<String> {
-    //     let posts: Vec<Value> = request_bulk_posts_from_api();
-    //     let mut feed: Vec<String> = Vec::with_capacity(config::POSTS_PER_REQUEST);
-    //     for post in posts {
-    //         let at_uri: &str = post["post"]["uri"].as_str().unwrap();
-    //         let http_url: String = convert_at_uri_to_url(at_uri);
-    //         println!("this is a post url {:#?}", http_url);
-    //         feed.push(http_url);
-    //     }
-    //     feed
-    // }
+feed
 }
 
 fn main() {
     let total_posts: usize = get_posts_number();
-
-    collect_api_responses(total_posts);
+    println!("there are {} posts to request", total_posts);
+    let feed_urls = collect_api_responses(total_posts);
+    println!("collected {} posts", feed_urls.len());
 }
