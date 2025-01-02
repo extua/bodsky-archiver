218efbf49e14d79792d81f13d3981cc2700827e0
Pierre Marshall <pierre.marshall@bodleian.ox.ac.uk>
2025-01-02 14:41:13 +0000

    chore: pass parameters into list_repos function
    this returns some account ids!

--- src/bin/get_accounts.rs
+++ src/bin/get_accounts.rs
@@ -1,4 +1,7 @@
-use atrium_api::agent::{store::MemorySessionStore, AtpAgent};
+use atrium_api::{
+    agent::{store::MemorySessionStore, AtpAgent},
+    com::atproto::sync::list_repos::ParametersData,
+};
 use atrium_xrpc_client::reqwest::ReqwestClient;
 use std::env;
 
@@ -12,12 +15,19 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let password: String = env::var("BSKY_PASSWORD").expect("Password must be set in .env file.");
     let user_id: String = env::var("BSKY_USER").expect("User id must be set in .env file.");
     agent.login(user_id, password).await?;
+
     let result = agent
         .api
         .com
         .atproto
-        .server
-        .get_session()
+        .sync
+        .list_repos(
+            ParametersData {
+                cursor: None,
+                limit: None,
+            }
+            .into(),
+        )
         .await?;
     println!("{:?}", result);
     Ok(())
