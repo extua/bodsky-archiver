e3b264da10f573b19102397f86897aa89079f0cc
Pierre Marshall <pierre.marshall@bodleian.ox.ac.uk>
2025-01-16 10:41:55 +0000

    feat: create twitter client, cf. #13

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,11 @@
+#![allow(unused_imports)]
+#![allow(dead_code)]
 use bluesky::get_bluesky_posts;
+use twitter::get_twitter_posts;
 mod bluesky;
+mod twitter;
 
 fn main() {
-    get_bluesky_posts()
+    // get_bluesky_posts()
+    get_twitter_posts();
 }
---
+++ src/twitter.rs
@@ -0,0 +1,23 @@
+use reqwest::{header, Client};
+use std::{env, fmt::Error};
+
+fn create_twitter_client() -> Client {
+    const APP_USER_AGENT: &str = concat!(env!("CARGO_PKG_NAME"), "/", env!("CARGO_PKG_VERSION"),);
+    let mut headers = header::HeaderMap::new();
+    dotenvy::dotenv().expect("Something wrong with dotenvy");
+    let bearer_token: String =
+        env::var("TWITTER_API_BEARER_TOKEN").expect("Unable to read the bearer token from .env");
+    headers.insert(
+        "Authorization",
+        header::HeaderValue::from_str(&bearer_token)
+            .expect("Unable to insert bearer token into HeaderValue"),
+    );
+    let app_client = reqwest::Client::builder()
+        .user_agent(APP_USER_AGENT)
+        .default_headers(headers)
+        .build()
+        .expect("unable to create client");
+    app_client
+}
+
+pub fn get_twitter_posts() {}
