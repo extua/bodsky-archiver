139357e22596bed54187cf460a3e95e322387a2a
Pierre Marshall <pierre.marshall@bodleian.ox.ac.uk>
2025-02-05 15:49:31 +0000

    refactor: move request_tweets_from_api into its own function
    this needs fixing to deal with async stuff

--- src/twitter.rs
+++ src/twitter.rs
@@ -35,66 +35,61 @@ impl TwitterClient {
     }
 }
 
-fn collect_api_responses() -> Vec<String> {
+#[tokio::main]
+async fn request_tweets_from_api(app_client: Client, endpoint: Url) -> Response {
+    let mut retries: u8 = 0;
+    let mut backoff: Duration = Duration::from_secs(1);
+
+    let endpoint_str: &str = endpoint.as_str();
+
+    let response_from_retry: Result<Response, reqwest::Error> = loop {
+        match app_client
+            .get(endpoint_str)
+            .send()
+            .await
+            // if status is 429, back off and retry
+        {
+            Ok(resp) if resp.status().is_success() => break Ok(resp),
+            Ok(resp) if resp.status() == StatusCode::TOO_MANY_REQUESTS && retries < 6 => {
+                sleep(backoff).await;
+                retries += 1;
+                backoff *= 2;
+            }
+            // get the retry-after header value, convert it
+            // to seconds, then to duration, etc.
+            Ok(resp) if resp.headers().contains_key("retry-after") && retries < 6 => {
+                if let Some(retry_after) = resp.headers().get(RETRY_AFTER) {
+                    if let Ok(retry_after) = retry_after.to_str() {
+                        if let Ok(retry_after) = retry_after.parse::<u64>() {
+                            if retry_after < 128 {
+                                backoff = Duration::from_secs(retry_after + 1);
+                            }
+                        }
+                    }
+                }
+                sleep(backoff).await;
+                retries += 1;
+                backoff *= 2;
+            }
+            Err(e) => break Err(e),
+            // Breaking out with an error is fine,
+            // the last match arm should never be met
+            _ => panic!("Failed to request profile from API"),
+        }
+    };
+    // first error handiing on the response
+    let response: Response = match response_from_retry {
+        Ok(response) => response,
+        Err(network_error) => panic!("Failed to get API response: {network_error:?}"),
+    };
+    response
+}
+
+async fn collect_api_responses() -> Vec<String> {
     #[derive(Serialize, Deserialize)]
     #[serde(rename_all = "camelCase")]
     pub struct TweetFeed {
         data: Vec<Value>,
-    }
-
-    #[tokio::main]
-    async fn request_tweets_from_api(app_client: Client, endpoint: Url) -> TweetFeed {
-        let mut retries: u8 = 0;
-        let mut backoff: Duration = Duration::from_secs(1);
-
-        let endpoint_str: &str = endpoint.as_str();
-
-        let response_from_retry: Result<Response, reqwest::Error> = loop {
-            match app_client
-                .get(endpoint_str)
-                .send()
-                .await
-                // if status is 429, back off and retry
-            {
-                Ok(resp) if resp.status().is_success() => break Ok(resp),
-                Ok(resp) if resp.status() == StatusCode::TOO_MANY_REQUESTS && retries < 6 => {
-                    sleep(backoff).await;
-                    retries += 1;
-                    backoff *= 2;
-                }
-                // get the retry-after header value, convert it
-                // to seconds, then to duration, etc.
-                Ok(resp) if resp.headers().contains_key("retry-after") && retries < 6 => {
-                    if let Some(retry_after) = resp.headers().get(RETRY_AFTER) {
-                        if let Ok(retry_after) = retry_after.to_str() {
-                            if let Ok(retry_after) = retry_after.parse::<u64>() {
-                                if retry_after < 128 {
-                                    backoff = Duration::from_secs(retry_after + 1);
-                                }
-                            }
-                        }
-                    }
-                    sleep(backoff).await;
-                    retries += 1;
-                    backoff *= 2;
-                }
-                Err(e) => break Err(e),
-                // Breaking out with an error is fine,
-                // the last match arm should never be met
-                _ => panic!("Failed to request profile from API"),
-            }
-        };
-        // first error handiing on the response
-        let response: Response = match response_from_retry {
-            Ok(response) => response,
-            Err(network_error) => panic!("Failed to get API response: {network_error:?}"),
-        };
-        // parse the response into tweet struct
-        let parsed_response: TweetFeed = match response.json::<TweetFeed>().await {
-            Ok(response) => response,
-            Err(parse_error) => panic!("Failed to parse API response: {parse_error:?}"),
-        };
-        parsed_response
     }
 
     let twitter_client: Client = match TwitterClient::new() {
@@ -104,7 +99,11 @@ fn collect_api_responses() -> Vec<String> {
     println!("{:?}", twitter_client);
     let endpoint = Url::parse_with_params("https://api.x.com/2/tweets/search/recent",
                                  &[("query", "Oxford"), ("max_results", "10"), ("tweet.fields", "created_at,id,note_tweet")]).unwrap();
-    let bulk_posts = request_tweets_from_api(twitter_client, endpoint);
+    let response = request_tweets_from_api(twitter_client, endpoint);
+
+    // parse the response into tweet struct
+    let bulk_posts: TweetFeed = serde_json::from_str(response.text().await)?;
+
 
     let mut feed: Vec<String> = Vec::with_capacity(10);
 
