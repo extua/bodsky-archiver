a0383cca35b96b07929a052d62b9dd74560cce45
Pierre Marshall <pierre.marshall@gmail.com>
2025-02-12 11:30:35 +0000

    chore: move unused imports flag to twitter and await it too

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,12 @@
-#![allow(unused_imports)]
-#![allow(dead_code)]
 mod bluesky;
 use bluesky::get_bluesky_posts;
 mod twitter;
+#[allow(unused_imports)]
+#[allow(dead_code)]
 use twitter::get_twitter_posts;
 
 #[tokio::main]
 async fn main() {
     get_bluesky_posts().await;
-    // get_twitter_posts();
+    // get_twitter_posts().await;
 }
